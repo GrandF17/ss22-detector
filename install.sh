7779f1108084d0a46ad5a157c5c4d64f41172304
grandf17 <v.o.nikolenko@gmail.com>
2024-12-24 01:14:24 +0300

    added install.sh as basic intaller

--- install.sh
+++ install.sh
@@ -9,6 +9,8 @@ source myenv/bin/activate
 # installing dependecies:
 pip3 install scikit-learn pandas numpy graphviz
 
-# running script
-echo "Run 'deactivate' to exit the virtual environment"
-echo "Run 'source myenv/bin/activate & python3 ss_detector.py' to run GUI of ShadowSocks22 detector based on ML"
+# comand hints
+echo "
+Run 'source myenv/bin/activate ; python3 ss_detector.py' to run GUI of ShadowSocks22 detector based on ML
+Run 'deactivate' to exit the virtual environment
+"
